60181d0cb50913bc8294fca8f77eeca82e01c4d9
Marcos Paulo de Castro <marcospaulodc1@gmail.com>
2021-10-07 14:32:32 -0300

    Fix to project Unit Test

--- TestINSS/CalculoInssUnitTest.cs
+++ TestINSS/CalculoInssUnitTest.cs
@@ -2,24 +2,29 @@ namespace TestINSS
 {
     using INSS.Interfaces;
     using Moq;
+    using System;
     using Xunit;
 
     public class CalculoInssUnitTest
     {
-        private readonly Mock<IInssService> inssService;
+        //private readonly Mock<IInssService> inssService;
 
         public CalculoInssUnitTest()
         {
-            this.inssService = new Mock<IInssService>();
+            //this.inssService = new Mock<IInssService>();
         }
 
         [Fact(DisplayName ="Teste Cáculo INSS")]
-        public void Calcular_Aliquota_INSS()
+        public void Calcular_Salario_Ano_RetornaAliquotaINSS()
         {
             string ano = "2011";
-            decimal salario = 1000.00M;
+            decimal salario = 4000.00M;
+            decimal valorEsperado = 405.86M;
 
-            inssService.Setup(m => m.CalculaInssByYear(ano, salario));
+            var inssService = new INSS.Validators.InssValidator();
+            decimal result = inssService.CalculaInssByYear(ano, salario);
+
+            Assert.Equal(valorEsperado, result, 2);
         }
     }
 }
